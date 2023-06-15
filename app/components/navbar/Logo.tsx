3455c0be965d2af414368d5e822aeab8b13b1907
Thomas Booth <thomasbooth26@gmail.com>
2023-06-16 00:42:29 +0100

    remove breathing

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -5,14 +5,6 @@ import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
-  const breathing = {
-    scale: {
-      duration: 1,
-      repeat: Infinity,
-      repeatType: 'reverse',
-      ease: 'easeOut'
-    }
-  }
 const Logo = () => {
     const router = useRouter()
   return (
@@ -20,8 +12,6 @@ const Logo = () => {
       <motion.a
         drag
         className = 'text-3xl font-black'
-        animate = {{ scale: [1, 1.1]}}
-        transition = {breathing}
         dragConstraints={{ left: 0, right: 300, top: -60, bottom: 60 }}
         href="/"
         onClick={(e) => {
