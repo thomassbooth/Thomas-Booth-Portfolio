00094f0c3de948ae5cd53ac20b1dd5864134ff02
Thomas Booth <thomasbooth26@gmail.com>
2023-06-18 18:38:50 +0100

    page width change

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ import TechClient from './TechClient'
 
 const AboutPage = () => {
   return (
-    <PageTransition width = 'w-4/6'>
+    <PageTransition width = 'w-3/5'>
        <Title title = 'About me' pageNo = {'02'} slideDirection = {'right'}/>
        <p>
             My name is Thomas Booth and I am a full stack Software Engineer from the UK currently relocating to Dubai.
@@ -14,6 +14,7 @@ const AboutPage = () => {
         <p>
             I have worked with a wide variety of different technologies; front-end, back-end and cloud infastructure.
         </p>
+        <p>Lorem ipsum dolor sit amet. Ut laudantium animi ut laudantium enim et consequatur aspernatur nam molestiae repudiandae aut sunt minus ad similique vitae. 33 illum sint ea maiores quas est deleniti minima. </p><p>Non doloribus dolor et quisquam aliquam ea velit ipsam At corporis sint! Aut expedita dolorem sed dolore voluptatem et sequi nihil qui molestias pariatur. </p><p>Et sapiente dolor ut sequi velit non quibusdam omnis ut tenetur rerum sed voluptates deleniti. Et ducimus magni qui molestiae molestiae aut placeat molestias sed expedita dolores hic nemo galisum. Aut eius exercitationem hic iste nihil ea accusantium porro et illo suscipit est odio saepe hic autem natus? </p><p>Et reprehenderit sunt ab perspiciatis voluptas et quia voluptas. Ex Quis repellat ea distinctio numquam et veniam voluptas aut laboriosam velit et inventore galisum ut culpa rerum. </p><p>Ex ducimus soluta sit velit excepturi aut repudiandae nihil non atque delectus ut eaque minima. Eos facere nobis id quas fugiat nam eveniet sint aut quidem numquam est tempore reprehenderit est sunt quidem? Et cumque cumque a alias enim in voluptatem quod est nesciunt maxime et temporibus possimus. A nihil voluptatibus et doloremque aliquam sed maiores sint sit error quibusdam. </p><p>Est dolores rerum et voluptatem modi et quidem dolorum in aspernatur voluptatem. Est veniam sint qui veniam cumque cum Quis laborum. </p><p>A commodi pariatur et enim sapiente vel internos aliquid aut deserunt animi ea quod laborum et soluta saepe! Ea quia laboriosam est quia fugiat et explicabo quam sit blanditiis beatae est doloribus galisum. </p>
         <h2 className = 'font-bold text-xl m-5'>Recent technologies I&apos;ve been working with:</h2>
         <TechClient/>
     </PageTransition>
