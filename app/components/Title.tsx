a348c81fb456a5fee77080fa24baa094dfeea9ec
Thomas Booth <thomasbooth26@gmail.com>
2023-06-22 21:01:50 +0100

    better sizing

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -4,7 +4,6 @@ import { Inter, Roboto, Roboto_Mono } from 'next/font/google'
 import { motion } from 'framer-motion'
 const RobotoMono = Roboto_Mono({ weight: '400', subsets: ['latin'] })
 
-const inter = Inter({weight: ['500'], subsets: ['latin'] })
 
 interface titleProps {
     title: string
@@ -37,7 +36,7 @@ const Title: React.FC<titleProps> = ({title, pageNo, slideDirection}) => {
   return (
     <>
         <motion.h1 
-          className = {`${inter.className} font-black text-4xl mb-10 flex items-center gap-4`}
+          className = {`font-bold text-5xl mb-10 flex items-center tracking-wider gap-4`}
             initial = {initialAnimation}
             animate = {{opacity: 1, x: '0%', y: '0%'}}
             transition = {{ delay: 1}}>
--- app/components/about/MeClient.tsx
+++ app/components/about/MeClient.tsx
@@ -1,30 +1,41 @@
 'use client'
 
+import { useScroll, motion } from 'framer-motion';
 import Image from 'next/image'
 import React from 'react'
 
 const MeClient = () => {
-  return (
-    <div className = 'flex h-[70vh] 2xl:h-[75vh] text-lg'>
-      <div className = 'leading-relaxed px-4'>
-        <p className = 'mb-4'>
-            My name is Thomas Booth and I am a full stack Software Engineer from the UK currently relocating to Dubai.
-        </p>
-        <p>
-          Ive amassed considerable experience in diverse tech areas, encompassing front-end, back-end, and cloud infrastructure. My present work involves utilizing technologies like Nextjs, React, Nodejs, Django, and a broad spectrum of AWS tools.
 
-          My proficiency extends beyond the tech sector. I earned a solid First class degree in Physics and Mathematics during my tenure at Cardiff University. And interestingly, my foray into the professional world started early. From the age of 13, I embarked on my journey as a freelance graphic designer, successfully completing large-scale projects for businesses and YouTube personalities.
-        </p>
+  const { scrollYProgress } = useScroll();
+
+  return (
+    <section>
+      <div className = 'flex h-[70vh] 2xl:h-[75vh] text-lg'>
+        <div className = 'leading-relaxed px-4'>
+          <p className = 'mb-4'>
+              My name is Thomas Booth and I am a full stack Software Engineer from the UK currently relocating to Dubai.
+          </p>
+          <p>
+            Ive amassed considerable experience in diverse tech areas, encompassing front-end, back-end, and cloud infrastructure. My present work involves utilizing technologies like Nextjs, React, Nodejs, Django, and a broad spectrum of AWS tools.
+
+            My proficiency extends beyond the tech sector. I earned a solid First class degree in Physics and Mathematics during my tenure at Cardiff University. And interestingly, my foray into the professional world started early. From the age of 13, I embarked on my journey as a freelance graphic designer, successfully completing large-scale projects for businesses and YouTube personalities.
+          </p>
+        </div>
+        <div className = 'relative group'>
+          <div className = 'group-hover:translate-y-1 group-hover:translate-x-1 transition-all absolute border-2 rounded-md border-pastel-brown-light transform translate-x-2 translate-y-2 -z-10'/>
+            <Image src = '/images/dummyMe.jpg' 
+            className = 'shadow-sm'
+            alt = 'Pick of me :D'
+            width={500}
+            height={500}/>
+        </div>
       </div>
-      <div className = 'relative group'>
-        <div className = 'group-hover:translate-y-1 group-hover:translate-x-1 transition-all absolute border-2 rounded-md border-pastel-brown-light transform translate-x-2 translate-y-2 -z-10'/>
-          <Image src = '/images/dummyMe.jpg' 
-          className = 'shadow-sm'
-          alt = 'Pick of me :D'
-          width={500}
-          height={500}/>
+      <div className = 'flex flex-col text-6xl'>
+        <motion.span style = {{ transform: `translate(${scrollYProgress}px)`}}>DUBAI LONDON RKV</motion.span>
+        <span>RKV LONDON DUBAI</span>
       </div>
-    </div>
+      <motion.div className = 'bg-black h-10' style={{ scaleX: scrollYProgress }} />
+    </section>
   )
 }
 
