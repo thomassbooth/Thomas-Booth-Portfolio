7d1b5a43c5121b73fbc0a7994d984e9aacc12053
Thomas Booth <thomasbooth26@gmail.com>
2023-06-25 00:20:39 +0100

    cities

--- components/about/Cities.tsx
+++ components/about/Cities.tsx
@@ -14,45 +14,46 @@ import { wrap } from "@motionone/utils";
 interface ParallaxProps {
   children: string;
   baseVelocity: number;
+  className?: string
 }
 
-function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
-  const baseX = useMotionValue(0);
-  const { scrollY } = useScroll();
-  const scrollVelocity = useVelocity(scrollY);
-  const smoothVelocity = useSpring(scrollVelocity, {
-    damping: 50,
-    stiffness: 400
-  });
-  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-    clamp: false
-  });
+function ParallaxText({ children, baseVelocity = 100, className }: ParallaxProps) {
+  // const baseX = useMotionValue(0);
+  // const { scrollY } = useScroll();
+  // const scrollVelocity = useVelocity(scrollY);
+  // const smoothVelocity = useSpring(scrollVelocity, {
+  //   damping: 50,
+  //   stiffness: 400
+  // });
+  // const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+  //   clamp: false
+  // });
 
-  /**
-   * This is a magic wrapping for the length of the text - you
-   * have to replace for wrapping that works for you or dynamically
-   * calculate
-   */
-  const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+  // /**
+  //  * This is a magic wrapping for the length of the text - you
+  //  * have to replace for wrapping that works for you or dynamically
+  //  * calculate
+  //  */
+  // const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
 
-  const directionFactor = useRef<number>(1);
-  useAnimationFrame((t, delta) => {
-    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+  // const directionFactor = useRef<number>(1);
+  // useAnimationFrame((t, delta) => {
+  //   let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
 
-    /**
-     * This is what changes the direction of the scroll once we
-     * switch scrolling directions.
-     */
-    if (velocityFactor.get() < 0) {
-      directionFactor.current = -1;
-    } else if (velocityFactor.get() > 0) {
-      directionFactor.current = 1;
-    }
+  //   /**
+  //    * This is what changes the direction of the scroll once we
+  //    * switch scrolling directions.
+  //    */
+  //   if (velocityFactor.get() < 0) {
+  //     directionFactor.current = -1;
+  //   } else if (velocityFactor.get() > 0) {
+  //     directionFactor.current = 1;
+  //   }
 
-    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+  //   moveBy += directionFactor.current * moveBy * velocityFactor.get();
 
-    baseX.set(baseX.get() + moveBy);
-  });
+  //   baseX.set(baseX.get() + moveBy);
+  // });
 
   /**
    * The number of times to repeat the child text should be dynamically calculated
@@ -63,7 +64,8 @@ function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
    */
   return (
     <div className="flex">
-      <motion.div className="flex flex-row gap-2" style={{ x: -500 }}>
+      <motion.div className={`flex flex-row gap-2 `} style={{ x: -300 }}>
+        <span>{children} </span>
         <span>{children} </span>
         <span>{children} </span>
         <span>{children} </span>
