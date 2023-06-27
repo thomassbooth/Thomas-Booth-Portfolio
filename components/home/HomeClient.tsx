414a41354b1a74fcbdcb147576cbdecd9309f853
Thomas Booth <thomasbooth26@gmail.com>
2023-06-27 23:10:40 +0100

    optimising images

--- components/home/HomeClient.tsx
+++ components/home/HomeClient.tsx
@@ -33,7 +33,7 @@ const HomeClient = () => {
       <motion.div
         
         animate = {{opacity: 1}}>
-          <Image className = '-z-10 opacity-10 absolute top-0' src = '/polygon-scatter-haikei.svg' width = {900} height = {675} alt = ''/>
+          <Image className = '-z-10 opacity-10 absolute right-0 top-0' priority src = '/polygon-scatter-haikei.svg' width = {900} height = {675} alt = ''/>
           <Title line1 = {'Hi.'} line2 = 'I&#39;m Thomas Booth.' />
           <p className = 'text-left mt-5 text-lg'>
           Welcome to my digital hub! As an experienced software engineer, I passionately craft functional, intuitive, and visually appealing digital spaces that encapsulate each client&#39;s brand spirit.
