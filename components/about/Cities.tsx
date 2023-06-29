af1b1634ac7beb8d5e73c54ad9303de01f834e8b
Thomas Booth <thomasbooth26@gmail.com>
2023-06-29 23:06:44 +0100

    scrolling better

--- components/about/Cities.tsx
+++ components/about/Cities.tsx
@@ -13,66 +13,21 @@ import { wrap } from "@motionone/utils";
 
 interface ParallaxProps {
   children: string;
-  baseVelocity: number;
-  className?: string
+  inverse: boolean
 }
 
-function ParallaxText({ children, baseVelocity = 100, className }: ParallaxProps) {
-  // const baseX = useMotionValue(0);
-  // const { scrollY } = useScroll();
-  // const scrollVelocity = useVelocity(scrollY);
-  // const smoothVelocity = useSpring(scrollVelocity, {
-  //   damping: 50,
-  //   stiffness: 400
-  // });
-  // const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-  //   clamp: false
-  // });
+function ParallaxText({ children, inverse}: ParallaxProps) {
 
-  // /**
-  //  * This is a magic wrapping for the length of the text - you
-  //  * have to replace for wrapping that works for you or dynamically
-  //  * calculate
-  //  */
-  // const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+  const { scrollYProgress } = useScroll()
+  let range = inverse ? [0, 1] : [1, 0]
+  const x = useTransform(scrollYProgress, range, [0, -600])
 
-  // const directionFactor = useRef<number>(1);
-  // useAnimationFrame((t, delta) => {
-  //   let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
-
-  //   /**
-  //    * This is what changes the direction of the scroll once we
-  //    * switch scrolling directions.
-  //    */
-  //   if (velocityFactor.get() < 0) {
-  //     directionFactor.current = -1;
-  //   } else if (velocityFactor.get() > 0) {
-  //     directionFactor.current = 1;
-  //   }
-
-  //   moveBy += directionFactor.current * moveBy * velocityFactor.get();
-
-  //   baseX.set(baseX.get() + moveBy);
-  // });
-
-  /**
-   * The number of times to repeat the child text should be dynamically calculated
-   * based on the size of the text and viewport. Likewise, the x motion value is
-   * currently wrapped between -20 and -45% - this 25% is derived from the fact
-   * we have four children (100% / 4). This would also want deriving from the
-   * dynamically generated number of children.
-   */
   return (
     <div className="flex">
-      <motion.div className={`flex flex-row gap-2`} style={{ x: -400 }}>
-        <span>{children} </span>
-        <span>{children} </span>
-        <span>{children} </span>
-        <span>{children} </span>
-        <span>{children} </span>
-        <span>{children} </span>
-        <span>{children} </span>
-        <span>{children} </span>
+      <motion.div className={`flex flex-row gap-2`} style={{ x }}>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
       </motion.div>
     </div>
   );
@@ -80,9 +35,9 @@ function ParallaxText({ children, baseVelocity = 100, className }: ParallaxProps
 
 export default function Cities() {
   return (
-    <section className = '2xl:py-44 py-20 font-extrabold'>
-      <ParallaxText baseVelocity={-0.5}> DUBAI LONDON </ParallaxText>
-      <ParallaxText baseVelocity={0.5}> LONDON DUBAI </ParallaxText>
+    <section className = '2xl:py-44 py-20 text-6xl font-extrabold'>
+      <ParallaxText inverse = {true} > DUBAI LONDON </ParallaxText>
+      <ParallaxText inverse = {false} > LONDON DUBAI </ParallaxText>
     </section>
   );
 }
