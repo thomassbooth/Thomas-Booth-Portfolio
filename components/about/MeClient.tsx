5721ef0b2c6b5bcd6faed32270599d9b0333afc7
Thomas Booth <thomasbooth26@gmail.com>
2023-07-01 22:24:54 +0100

    update to size

--- components/about/MeClient.tsx
+++ components/about/MeClient.tsx
@@ -29,12 +29,11 @@ const MeClient = () => {
           </p>
         </div>
           <Image src = '/images/thomasbooth01.jpeg' 
-              className = 'translate-x-10 shadow-md m-5 border-2 rounded-sm grayscale hover:grayscale-0 hover:translate-y-2 transition-all duration-500'
+              className = 'translate-x-10 shadow-md m-5 border-2 rounded-lg grayscale hover:grayscale-0 hover:translate-y-2 transition-all duration-500'
               alt = 'Pick of me :D'
               priority
               width={400}
               height={400}/>
-        
       </div>
       <div className = 'flex-col z-0 w-screen text-7xl text-pastel-brown-dark opacity-20 hidden lg:flex'>
         <Cities />
