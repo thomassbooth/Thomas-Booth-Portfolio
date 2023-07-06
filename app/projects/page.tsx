1bade34bb9dd26129eb29496b2a2b7eef3be5e6c
Thomas Booth <thomasbooth26@gmail.com>
2023-07-06 21:25:16 +0100

    images upload

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import PageTransition from "../../components/PageTransition"
 import Title from "../../components/Title"
+import Image from "next/image"
 
 export const metadata = {
   title: 'Projects - Thomas Booth',
@@ -12,6 +13,12 @@ const ProjectsPage = () => {
     <PageTransition width = 'w-4/5'>
       <Title title = 'Projects' pageNo = {'03'} slideDirection = {'down'}/>
       <Link href = '/projects/dave'>DAVE PROJECT</Link>
+      <Image 
+        className = 'border-2 rounded-lg'
+        src = '/images/airbnb.jpeg' 
+        alt = 'airbnb' 
+        width = {500} 
+        height = {500}/>
     </PageTransition>
   )
 } 
