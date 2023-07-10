0bbbe497ac0ca183305f10502326370f6b7f3ba1
Thomas Booth <thomasbooth26@gmail.com>
2023-07-10 22:12:01 +0100

    project update

--- components/projects/Intro.tsx
+++ components/projects/Intro.tsx
@@ -7,7 +7,7 @@ const Intro = () => {
     <motion.div 
         initial={{ x: '50%', opacity: 0 }}
         animate={{ x: '0%', opacity: 1 }}
-        transition={{delay: 1, type: 'spring', when: 'beforeChildren', duration: 1, staggerChildren: 0.2}}
+        transition={{delay: 2, type: 'spring', when: 'beforeChildren', duration: 1, staggerChildren: 0.2}}
         className = 'flex flex-col gap-4 leading-relaxed px-4'>
         <p>
         Uncover my coding prowess through these key projects: my own portfolio, a recreation of Instagram, and a replicated Airbnb platform, each exhibiting unique facets of my technical capabilities.
